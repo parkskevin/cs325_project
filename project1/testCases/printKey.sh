a0ed22a009c45f2fa104e34a9823d234127fcb51
kevin <parkske@onid.oregonstate.edu>
2015-04-18 10:26:56 -0700

    created properly formatted test cases and a dumb script to print the key to these

---
+++ project1/testCases/printKey.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "***** Case 1 *****"
+echo "Subarray: 8, 1, 3, 3, 1, -1, -4, -6, 2, 8, 19"
+echo "Max sum: 34"
+echo
+echo "***** Case 2 *****"
+echo "Subarray: 2, 9, 8, 6, 5"
+echo "Max sum: 30"
+echo
+echo "***** Case 3 *****"
+echo "Subarray: 23, 24, -1, -7, -8, 19"
+echo "Max sum: 50"
+echo
+echo "***** Case 4 *****"
+echo "Subarray: 59, 26, -53, 58, 97"
+echo "Max sum: 187"
+echo
+echo "***** Case 5 *****"
+echo "Subarray: 3, 2, 1, 1"
+echo "Max sum: 7"
+echo
+echo "***** Case 6 *****"
+echo "Subarray: 12, 99, 99"
+echo "Max sum: 210"
+echo
+echo "***** Case 7 *****"
+echo "Subarray: 4, -1, 2, 1"
+echo "Max sum: 6"
+echo
+echo "***** Case 8 *****"
+echo "Subarray: (empty)"
+echo "Max sum: 0"
