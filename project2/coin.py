0bc31517cf96529ad361a260603359a19139f08a
kevin <parkske@onid.oregonstate.edu>
2015-05-01 22:39:26 -0700

    major refactor, starting to adopt some of proj2's constraints. Needs more work, but does some simple printing now

--- project2/coin.py
+++ project2/coin.py
@@ -5,7 +5,7 @@
 #class:  CS325
 #Assgn:  Project 2
 #Descr:  This program calculates the least amount of coins necessary
-#	 to fulfill a given integer value currency. 
+#	     to fulfill a given integer value currency. 
 #
 #Input:  //TODO
 #
@@ -14,147 +14,132 @@
 import sys #for maxint
 import timeit #for timings
 import argparse #cmd line arg parsing
-import rand #random file generator
 
 #constants
-INPUTFILE = "input.txt"
-OUTPUTFILE = "Results.txt"
+INPUTFILE = "Amount.txt"
+OUTPUTFILE = "change.txt"
 
-def a1(arr):
+def a1(arr, value):
 #TODO changeslow
-def a2(arr):
+	return arr
+def a2(arr, value):
 #TODO greedy
-def a3(arr):
+	return arr
+def a3(arr, value):
 #TODO dynamicp
-	
-def printResults(arr, subArr, maxSum, mssTest, file):
-	if not(mssTest):
+	return arr
+
+#rough rewrite, will probably need update
+def printResults(arr, minCoins, outputDebug, file):
+	if(file):
+		file.write("Coin Array: %s\n" % arr)
+	if(outputDebug):
+		print("Coin Array: %s" % arr)
+	if (minCoins > 0):
 		if(file):
-			file.write("Original Array: %s\n" % arr)
-		else:
-			print("Original Array: %s" % arr)
-	if (maxSum > 0):
-		if(file):
-			file.write("Subarray: %s\n" % subArr)
-			file.write("Max Sum: %d\n" % maxSum)
-		else:
-			print("Subarray: %s" % subArr)
-			print("Max Sum: %d" % maxSum) 
+			file.write("Min Coins: %d\n" % minCoins)
+		if(outputDebug):
+			print("Min Coins: %d" % minCoins) 
 	else: 
 		if(file):
-			file.write("Subarray: (empty)\n")
-			file.write("Max Sum: 0   ****  Note at least one value in the" + \
-				   " array must be positive\n")
-		else:
-			print "Subarray: (empty)"
-			print "Max Sum: 0   ****  Note at least one value in the" + \
-				   " array must be positive"
+			file.write("Min Coins: 0   **** Value less than min coin");
+		if(outputDebug):
+			print "Min Coins: 0   **** Value less than min coin"
 	
+#TODO: reuse most of this, rename as necessary
 #cmd line args parser setup
-parser = argparse.ArgumentParser(description="Find maximum sum sub-array. No args does MSS_Results.txt test")
-parser.add_argument("-a", "--algo", type=int, help="The choice of algorithm(1-4)")
+parser = argparse.ArgumentParser(description="Find minimum coins")
+parser.add_argument("-a", "--algo", type=int, help="The choice of algorithm(1-3)")
 parser.add_argument("-f", "--file", help="Path to input file")
-parser.add_argument("-r", "--rand", type=int, help="Randomly generated")
 parser.add_argument("-t", "--timing", help="record timings", action="store_true")
+parser.add_argument("-d", "--debug", help="turn debug messages on")
 args = parser.parse_args()
 
 #cmd line args logic
-mssTest = False #whether we're printing to the MSS_Results.txt
-out = False #file handle to MSS_Results.txt
+outputDebug = True #whether we're printing debug
+out = False #file handle to change.txt
 
-#if not cmd line args, do MSS_Results.txt action
-if not (len(sys.argv) > 1):
-	mssTest = True
-	out = file(OUTPUTFILE, 'w')
+#handle cmd line args
+if not (len(sys.argv) > 2):
+	parser.error("No input file and/or algorithm specified")
 elif not (args.algo):
 	parser.error("No algorithm specified")
-elif (args.algo > 4 or args.algo < 1):
-	parser.error("Choose an algorithm 1-4")
-if (args.file and args.rand):
-	parser.error("Cannot do create random and open existing file. Choose one.")
+elif (args.algo > 3 or args.algo < 1):
+	parser.error("Choose an algorithm 1-3")
 elif (not args.file):
-	#no file, so random is an option
-	if(args.rand):
-		args.file = RANDFILE
-	else:
-		args.file = MSSFILE
+	args.file = INPUTFILE
+if (not args.debug):
+	outputDebug = False
 
-if not (mssTest):
+#setup output file
+outFileName = args.file.replace(".txt", "") + OUTPUTFILE
+out = open(outFileName, 'w')
+	
+if(outputDebug):
 	#debug
 	print "Input file: " + args.file
 	print "Selected algorithm: " + str(args.algo)
-
-#if args.rand is used, make the file first
-if(args.rand):
-	#needs some sanitizing funcs here
-	rand.make_rand(args.rand)
 
 #open whichever file as input
 f = open(args.file, 'r')
 
 #create the master list to hold each array as another list (2D array)
 #append each array as new list of ints for each line of text read from file
+#append the value we're interested in realizing to another array, the
+#same inde matching the demonination array and value
 testArr = []
+valueArr = []
 for i, line in enumerate(f):
-	testArr.append([])
-	testArr[i].append(map(int, line.replace("[", "").replace("]", "").replace("\n", "").split(',')))
+	if(i % 2 is 0):
+		testArr.append([])
+		testArr[i / 2].append(map(int, line.replace("[", "").replace("]", "").replace("\n", "").split(',')))
+	else:
+		valueArr.append([])
+		line.replace("\n", "")
+		valueArr[i / 2].append(line)
 f.close()
 
-#for each algorithm, find the subarray, calculate the sum, then print
-if (args.algo == 1 or mssTest):
+#for each algorithm, find the min coin per denomination value, calculate the sum, then print
+if (args.algo == 1):
 	if not (args.timing or isinstance(out, bool)):
-		out.write("******************** Algorithm 1 ********************\n")
-	for i in testArr:
+		out.write("******************** changeslow ********************\n")
+	for k, i in enumerate(testArr):
 		for j in i:
 			if (args.timing):
-				print(str(len(j))+", "+str(timeit.timeit(lambda:a1(j),number=1)))
+				print(str(len(j))+", "+str(timeit.timeit(lambda:a1(j, valueArr[k]),number=1)))
 			else:
-				subArray = a1(j)
-				totalSum = sum(subArray)
-				printResults(j, subArray, totalSum, mssTest, out)
-				if not (mssTest):
+				subArray = a1(j, valueArr[k])
+				minCoins = sum(subArray)
+				printResults(subArray, minCoins, outputDebug, out)
+				if not (outputDebug):
 					print("")
 
-if (args.algo == 2 or mssTest):
+if (args.algo == 2):
 	if not (args.timing or isinstance(out, bool)):
-		out.write("******************** Algorithm 2 ********************\n")
-	for i in testArr:
+		out.write("******************** changegreedy ********************\n")
+	for k, i in enumerate(testArr):
 		for j in i:
 			if (args.timing):
-				print(str(len(j))+", "+str(timeit.timeit(lambda:a2(j),number=1)))
+				print(str(len(j))+", "+str(timeit.timeit(lambda:a2(j, valueArr[k]),number=1)))
 			else:
-				subArray = a2(j)
-				totalSum = sum(subArray)
-				printResults(j, subArray, totalSum, mssTest, out)
-				if not (mssTest):
+				subArray = a2(j, valueArr[k])
+				minCoins = sum(subArray)
+				printResults(subArray, minCoins, outputDebug, out)
+				if not (outputDebug):
 					print("")
 
-if (args.algo == 3 or mssTest):
+if (args.algo == 3):
 	if not (args.timing or isinstance(out, bool)):
-		out.write("******************** Algorithm 3 ********************\n")
-	for i in testArr:
+		out.write("******************** changedp ********************\n")
+	for k, i in enumerate(testArr):
 		for j in i:
 			if (args.timing):
-				print(str(len(j))+", "+str(timeit.timeit(lambda:a3(j),number=1)))
+				print(str(len(j))+", "+str(timeit.timeit(lambda:a3(j, valueArr[k]),number=1)))
 			else:
-				subArray = a3(j)
-				totalSum = sum(subArray)
-				printResults(j, subArray, totalSum, mssTest, out)
-				if not (mssTest):
-					print("")
-
-if (args.algo == 4 or mssTest):
-	if not (args.timing or isinstance(out, bool)):
-		out.write("******************** Algorithm 4 ********************\n")
-	for i in testArr:
-		for j in i:
-			if (args.timing):
-				print(str(len(j))+", "+str(timeit.timeit(lambda:a4(j),number=1)))
-			else:
-				subArray = a4(j)
-				totalSum = sum(subArray)
-				printResults(j, subArray, totalSum, mssTest, out)
-				if not (mssTest):
+				subArray = a3(j, valueArr[k])
+				minCoins = sum(subArray)
+				printResults(subArray, minCoins, outputDebug, out)
+				if not (outputDebug):
 					print("")
 
 if (out):
