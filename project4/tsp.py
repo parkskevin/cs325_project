9202bc8c0f777e4d5c4bf2e15d7b1bece9b23f11
danielsbonnin <danielsbonnin@gmail.com>
2015-05-30 15:39:05 -0700

    tsp.py is ready for logic in calcTSP function

--- project4/tsp.py
+++ project4/tsp.py
@@ -16,6 +16,8 @@
 import sys #for maxint
 import timeit #for timings
 import math #for sqrt and round
+import argparse #for parsing args
+import re #for regex
 
 #constants
 INPUTFILE = "Amount.txt"
@@ -31,108 +33,42 @@ def distance(a,b):
     #return int(math.sqrt(dx*dx + dy*dy)+0.5) # equivalent to the next line
     return int(round(math.sqrt(dx*dx + dy*dy)))
 	
-	
+#Source: tsp-verifier.py from supplied project files
+def readinstance(filename):
+    # each line of input file represents a city given by three integers:
+    # identifier x-coordinate y-coordinate (space separated)
+    # city identifiers are always consecutive integers starting with 0
+    # (although this is not assumed here explicitly,
+    #    it will be a requirement to match up with the solution file)
+    f = open(filename,'r')
+    line = f.readline()
+    cities = []
+    while len(line) > 1:
+        lineparse = re.findall(r'[^,;\s]+', line)
+        cities.append([int(lineparse[1]),int(lineparse[2])])
+        line = f.readline()
+    f.close()
+    return cities
+
+def calcTsp(cities):
+	#Input: an array of x, y cartesian coordinates
+	#Output: an approximate shortest path between input coordinates
+	print str(cities)
+
 #cmd line args parser setup
-parser = argparse.ArgumentParser(description="Find minimum coins")
-parser.add_argument("-a", "--algo", type=int, help="The choice of algorithm(1-3)")
-parser.add_argument("-f", "--file", help="Path to input file")
-parser.add_argument("-t", "--timing", help="record timings", action="store_true")
-parser.add_argument("-d", "--debug", help="turn debug messages on", action="store_true")
+parser = argparse.ArgumentParser(description="Enter an input file path")
+parser.add_argument("inputFile", type=str, help="Path to input file")
 args = parser.parse_args()
 
-#cmd line args logic
-outputDebug = True #whether we're printing debug
-out = False #file handle to change.txt
-coinTest = False #whether this is a test of all algos
-canDoSlow = True #whether we can perform slowchange algo
-
-#handle cmd line args
-if not (len(sys.argv) > 2):
-	parser.error("Too few arguments")
-elif not (args.algo):
-	coinTest = True #do all the algorithms then
-elif (args.algo and args.algo > 3 or args.algo < 1):
-	parser.error("Choose an algorithm 1-3")
-elif (not args.file):
-	parser.error("Missing file argument")
-if (not args.debug):
-	outputDebug = False
 
 #setup output file
-outFileName = args.file.replace(".txt", "") + OUTPUTFILE
+outFileName = args.inputFile + ".tour"
 out = open(outFileName, 'w')
-	
-if(outputDebug):
-	#debug
-	print "Input file: " + args.file
-	print "Selected algorithm: " + str(args.algo)
 
-#open whichever file as input
-f = open(args.file, 'r')
+#parse cities into a list of lists.
+cities = readinstance(args.inputFile)
 
-#create the master list to hold each array as another list (2D array)
-#append each array as new list of ints for each line of text read from file
-#append the value we're interested in realizing to another array, the
-#same index matching the demonination array and value
-testArr = []
-valueArr = []
-for i, line in enumerate(f):
-	if(i % 2 is 0):
-		testArr.append([])
-		testArr[i / 2].append(map(int, line.replace("[", "").replace("]", "").replace("\n", "").split(',')))
-	else:
-		valueArr.append([])
-		valueArr[i / 2].append(int(line.replace("\n", "")))
-f.close()
-
-#for each algorithm, find the min coin per denomination value, calculate the sum, then print
-if (args.algo == 1 or coinTest == True):
-	if not (args.timing or isinstance(out, bool)):
-		out.write("******************** changeslow ********************\n")
-	for k, i in enumerate(testArr):
-		for j in i:
-			if (valueArr[k][0] > SLOWMAX):
-				if(outputDebug):
-					print "Output %d too large" % valueArr[k][0]
-				if(file):
-					out.write("Output %d too large\n" % valueArr[k][0])
-				continue
-			if (args.timing):
-				print(str(valueArr[k][0])+", "+str(len(j))+", "+str(timeit.timeit(lambda:a1(j, valueArr[k][0]),number=1)))
-			else:
-				subArray = a1(j, valueArr[k][0])
-				minCoins = sum(subArray)
-				printResults(subArray, minCoins, outputDebug, out)
-				if not (outputDebug):
-					out.write("")
-
-if (args.algo == 2 or coinTest == True):
-	if not (args.timing or isinstance(out, bool)):
-		out.write("******************** changegreedy ********************\n")
-	for k, i in enumerate(testArr):
-		for j in i:
-			if (args.timing):
-				print(str(valueArr[k][0])+", "+str(len(j))+", "+str(timeit.timeit(lambda:a2(j, valueArr[k][0]),number=1)))
-			else:
-				subArray = a2(j, valueArr[k][0])
-				minCoins = sum(subArray)
-				printResults(subArray, minCoins, outputDebug, out)
-				if not (outputDebug):
-					out.write("")
-
-if (args.algo == 3 or coinTest == True):
-	if not (args.timing or isinstance(out, bool)):
-		out.write("******************** changedp ********************\n")
-	for k, i in enumerate(testArr):
-		for j in i:
-			if (args.timing):
-				print(str(valueArr[k][0])+", "+str(len(j))+", "+str(timeit.timeit(lambda:a3(j, valueArr[k][0]),number=1)))
-			else:
-				subArray = a3(j, valueArr[k][0])
-				minCoins = sum(subArray)
-				printResults(subArray, minCoins, outputDebug, out)
-				if not (outputDebug):
-					out.write("")
+print(str(timeit.timeit(lambda:calcTsp(cities),number=1)))
 
 if (out):
 	out.close()
